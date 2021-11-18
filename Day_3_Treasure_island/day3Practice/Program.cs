9bd5cd07ee3169d16f2a35fa2f8101c5496baed0
kaly <kalimamadou@gmail.com>
2021-11-17 16:35:25 -0800

    100day of c# coding day 3 practice exercise BMI calculator

--- Day_3_Treasure_island/day3Practice/Program.cs
+++ Day_3_Treasure_island/day3Practice/Program.cs
@@ -23,6 +23,36 @@ namespace day3Practice
                 Console.WriteLine("This is an odd number.");
             }
 
+            Console.WriteLine("\n\n");
+
+            // Part 2 BMI calculator
+            Console.WriteLine("2. BMI Calculator 2");
+            Console.Write("Enter your height in m: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter you weight in kg: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            // Calculate BMI 
+            double bmi = weight / Math.Pow(height, 2);
+            //Console.WriteLine("Your BMI is {0}", Math.Round(bmi));
+            if (bmi < 18.5)
+            {
+                Console.WriteLine($"Your BMI is {Math.Round(bmi)}, you are underweight.");
+            } else if ( bmi < 25)
+            {
+                Console.WriteLine($"Your BMI is {Math.Round(bmi)}, you have a normal weight.");
+            } else if (  bmi < 30)
+            {
+                Console.WriteLine($"Your BMI is {Math.Round(bmi)}, you are slightly overweight.");
+            } else if ( bmi < 35)
+            {
+                Console.WriteLine($"Your BMI is {Math.Round(bmi)}, you are obese.");
+            } else
+            {
+                Console.WriteLine($"Your BMI is {Math.Round(bmi)}, you are clinically obese.");
+            }
+
+
             Console.ReadKey();
         }
     }
