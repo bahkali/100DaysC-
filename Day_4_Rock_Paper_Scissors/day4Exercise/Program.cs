001c3547e0a2a4534628728d938d7da961855842
kaly <kalimamadou@gmail.com>
2021-11-18 13:11:27 -0800

    100Day of Coding day 4 exercise  Head and tail app and Banker Roulette using random number selection

--- Day_4_Rock_Paper_Scissors/day4Exercise/Program.cs
+++ Day_4_Rock_Paper_Scissors/day4Exercise/Program.cs
@@ -10,6 +10,34 @@ namespace day4Exercise
     {
         static void Main(string[] args)
         {
+
+            Random rand = new Random();
+
+            // Exercise 1 - Heads and Tails
+            /*
+            Console.WriteLine("Heads and Tails games");
+            Console.WriteLine("Every time you run the program it will randomly choose between head and tail.");
+            
+            int randNumber= rand.Next(0, 2);
+            if (randNumber == 1)
+            {
+                Console.WriteLine("Heads");
+            } else {Console.WriteLine("Tails");}
+
+            */
+            // Exercise 2 - Banker Roulette
+
+
+            Console.WriteLine("Banker Roulette");
+            Console.WriteLine("Give me everybody's names, separated by a comma.");
+            string[] names = Console.ReadLine().Split(',');
+            
+      
+            int randName = rand.Next(0, names.Length + 1);
+            Console.WriteLine($"{names[randName].Trim(' ')} is going to buy the meal today!");
+
+            // End Program
+            Console.ReadKey();
         }
     }
 }
