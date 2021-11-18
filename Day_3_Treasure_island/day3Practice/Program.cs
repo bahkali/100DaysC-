3c89612d530de1d7f7aef14d927c900235908770
kaly <kalimamadou@gmail.com>
2021-11-17 16:10:04 -0800

    100 days of c# coding: day 3 practice coding odd and even.

---
+++ Day_3_Treasure_island/day3Practice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 1 Odd or Even
+            Console.WriteLine("1. Odd or Even exercise");
+            Console.Write("Please enter a number to check if even or odd? ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("This is an even number.");
+            }
+            else
+            {
+                Console.WriteLine("This is an odd number.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
