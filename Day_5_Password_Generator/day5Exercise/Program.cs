de4dcecbbc18e39defc3fab9dad406753e2e5fba
kaly <kalimamadou@gmail.com>
2021-11-19 13:39:31 -0800

    day 5 Exercise -  Highest score: using foreach loop to determine the higest number in the list.

--- Day_5_Password_Generator/day5Exercise/Program.cs
+++ Day_5_Password_Generator/day5Exercise/Program.cs
@@ -11,8 +11,9 @@ namespace day5Exercise
         static void Main(string[] args)
         {
             // Exercise 1 - Average Height
-
-            Console.WriteLine("Average Height");
+            /*
+            Console.WriteLine(" Average Height ");
+            Console.WriteLine("================");
             Console.Write("Input a list of student heights separated by space: ");
             // Get the data and split it
             string[] inputs = Console.ReadLine().Split(' ');
@@ -22,14 +23,33 @@ namespace day5Exercise
             {
                 student_heights.Add(Convert.ToInt32(s)) ;
             }
-
+            // calculate the total and average of heights
             int total_heights = student_heights.Sum();
             int average_heights = total_heights / student_heights.Count;
             Console.WriteLine($"The average height is {average_heights}.");
-
+            */
             // Exercise 2 - High Score
-
-
+            Console.WriteLine("   High Score ");
+            Console.WriteLine("================");
+            Console.Write("Input a list of student score separated by space: ");
+            // Get the data and split it
+            string[] inputs = Console.ReadLine().Split(' ');
+            List<int> student_score = new List<int>();
+            // Convert item to int
+            foreach (string s in inputs)
+            {
+                student_score.Add(Convert.ToInt32(s));
+            }
+            //int maxNumber = student_score.Max();
+            int maxNumber = 0;
+            foreach(int i in student_score)
+            {
+                if(maxNumber < i)
+                {
+                    maxNumber = i;
+                }
+            }
+            Console.WriteLine($"The highest score in the class is: {maxNumber}");
             // Exercise 3 - Adding Even Numbers
 
 
