d5910347b75e408a8cffad1e408e8c26c00d320c
kaly <kalimamadou@gmail.com>
2021-11-19 13:26:48 -0800

    Day 5 Exercise -  Average height Build a app using list and its function to determine the average height of an array of int.

--- Day_5_Password_Generator/day5Exercise/Program.cs
+++ Day_5_Password_Generator/day5Exercise/Program.cs
@@ -10,6 +10,33 @@ namespace day5Exercise
     {
         static void Main(string[] args)
         {
+            // Exercise 1 - Average Height
+
+            Console.WriteLine("Average Height");
+            Console.Write("Input a list of student heights separated by space: ");
+            // Get the data and split it
+            string[] inputs = Console.ReadLine().Split(' ');
+            List<int> student_heights = new List<int>();
+            // Convert item to int
+            foreach (string s in inputs)
+            {
+                student_heights.Add(Convert.ToInt32(s)) ;
+            }
+
+            int total_heights = student_heights.Sum();
+            int average_heights = total_heights / student_heights.Count;
+            Console.WriteLine($"The average height is {average_heights}.");
+
+            // Exercise 2 - High Score
+
+
+            // Exercise 3 - Adding Even Numbers
+
+
+            // Exercise 4 -  FizzBuzz Job
+
+            // End of Program
+            Console.ReadKey();
         }
     }
 }
