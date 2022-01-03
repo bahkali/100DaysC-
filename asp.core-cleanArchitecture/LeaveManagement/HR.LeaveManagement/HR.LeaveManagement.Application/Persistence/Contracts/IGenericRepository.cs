103f31a010789cbfb080a5c095c4ca63ff343721
kaly <kalimamadou@gmail.com>
2022-01-03 04:09:41 -0800

    LeaveManagement: Build the Application and its interface for the repository -  ILeaveAllocationRepository - ILeaveTypeRepository - ILeaveRequestRepositoy

---
+++ asp.core-cleanArchitecture/LeaveManagement/HR.LeaveManagement/HR.LeaveManagement.Application/Persistence/Contracts/IGenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Persistence.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        /*
+         * @param: id of item
+         * @return:
+         */
+        Task<T> Get(int id);
+
+        /* 
+         * @return: get all the data in readonlyList
+         */
+        Task<IReadOnlyList<T>> GetAll();
+        /*
+         * @param item
+         */
+        Task<T> Add(T entity);
+
+        /*
+         * @param item
+         */
+        Task<T> Update(T entity);
+
+        /*
+         * @param item
+         */
+        Task<T> Delete(T entity);
+    }
+}
---
+++ asp.core-cleanArchitecture/LeaveManagement/HR.LeaveManagement/HR.LeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
@@ -0,0 +1,10 @@
+﻿using HR.LeaveManagement.Domain;
+
+
+namespace HR.LeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
+    {
+
+    }
+}
