51af127c92229cba6cef86d38590523ef3aaca50
kaly <kalimamadou@gmail.com>
2022-01-03 03:52:25 -0800

    LeaveManagement: Build the Domain library with its entity - LeaveAllocation -LeaveRequest -LeaveType

---
+++ Day_12_BlackjacK/demo2/newtest2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace newtest2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Day_12_BlackjacK/demo2/nextOne/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace nextOne.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
